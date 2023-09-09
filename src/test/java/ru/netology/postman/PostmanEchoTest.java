b457e4b64cf0bb0f63182237b092e3c292676b2e
Ekaterina Saukova <esaukova@outlook.com>
2023-09-09 12:53:11 +0300

    corrected test

--- src/test/java/ru/netology/postman/PostmanEchoTest.java
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -7,16 +7,14 @@ import static org.hamcrest.Matchers.equalTo;
 public class PostmanEchoTest {
     @Test
     void testPostmanEcho() {
-        // Given - When - Then
-        //
         given()
                 .log().all()
                 .baseUri("https://postman-echo.com")
-                .body("some data") //
-        //
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
                 .when()
                 .post("/post")
-        //
+                .prettyPeek()
                 .then()
                 .statusCode(200)
                 .body("data", equalTo("some data"))
