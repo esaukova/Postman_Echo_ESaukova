17522a9e40845ee3f5d84e70864274ee0e0227d8
Ekaterina Saukova <esaukova@outlook.com>
2023-09-09 10:43:28 +0300

    add PostmanEchoTest

---
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology.postman;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void testPostmanEcho() {
+        // Given - When - Then
+        //
+        given()
+                .log().all()
+                .baseUri("https://postman-echo.com")
+                .body("some data") //
+        //
+                .when()
+                .post("/post")
+        //
+                .then()
+                .statusCode(200)
+                .body(equalTo("some value"))
+        ;
+    }
+}
